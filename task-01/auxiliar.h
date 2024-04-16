5472deee048f219e58025e258274d3a32741530c
tuildes <frehsegustavo@gmail.com>
2024-04-16 15:48:20 -0300

    [add] Funções de teste

--- task-01/auxiliar.h
+++ task-01/auxiliar.h
@@ -9,7 +9,7 @@
 #include <stdio.h>
 #include <math.h>
 
-#define MAX_VALOR_ALEAT 10
+#define MAX_VALOR_ALEAT 999999
 
 /*
     Imprime o VETOR no formato:
@@ -26,5 +26,20 @@ void aleatorizarVetor(int vetor[], size_t tam);
 
 /* Imprime os resultados de Index de Busca, comparações e tempo */
 void resultados(size_t idxBusca, uint64_t numComp, double tempo);
+
+/* 
+    Verifica se esta ordenado
+    Retorna 1 em caso de ERRO (não ordenado)
+*/
+unsigned short int testarOrdenacao(int vetor[], size_t tam);
+
+/* 
+    Verifica se a busca foi correta
+    Retorna 1 em caso de ERRO (index não confere)
+    Não verifica em caso de elemento não existente
+*/
+unsigned short int testarBusca(int vetor[], int valor, ssize_t index);
+
+unsigned short int testarBuscaImpossivel(int vetor[], ssize_t index);
 
 #endif  // AUXILIAR_H_
